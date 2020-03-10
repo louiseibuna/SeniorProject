88747ec0afd054c338288d85ec312868e16342f6
louiseibuna <louisemibuna@gmail.com>
2020-03-09 19:14:34 -0700

    pie chart added

--- metrics-dashboard/src/components/Dashboard/Chart.js
+++ metrics-dashboard/src/components/Dashboard/Chart.js
@@ -1,54 +1,31 @@
-import React from 'react';
-import { useTheme } from '@material-ui/core/styles';
-import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
-import Title from './Title';
+import React, { PureComponent } from 'react';
+import {
+  PieChart, Pie, Legend, Tooltip,
+} from 'recharts';
+import Title from './Title.js';
 
-// Generate Sales Data
-function createData(time, amount) {
-  return { time, amount };
-}
-
-const data = [
-  createData('00:00', 0),
-  createData('03:00', 300),
-  createData('06:00', 600),
-  createData('09:00', 800),
-  createData('12:00', 1500),
-  createData('15:00', 2000),
-  createData('18:00', 2400),
-  createData('21:00', 2400),
-  createData('24:00', undefined),
+const data01 = [
+  { name: 'Group A', value: 400 }, { name: 'Group B', value: 300 },
+  { name: 'Group C', value: 300 }, { name: 'Group D', value: 200 },
+  { name: 'Group E', value: 278 }, { name: 'Group F', value: 189 },
 ];
 
-export default function Chart() {
-  const theme = useTheme();
+const data02 = [
+  { name: 'Group A', value: 2400 }, { name: 'Group B', value: 4567 },
+  { name: 'Group C', value: 1398 }, { name: 'Group D', value: 9800 },
+  { name: 'Group E', value: 3908 }, { name: 'Group F', value: 4800 },
+];
 
-  return (
-    <React.Fragment>
-      <Title>Today</Title>
-      <ResponsiveContainer>
-        <LineChart
-          data={data}
-          margin={{
-            top: 16,
-            right: 16,
-            bottom: 0,
-            left: 24,
-          }}
-        >
-          <XAxis dataKey="time" stroke={theme.palette.text.secondary} />
-          <YAxis stroke={theme.palette.text.secondary}>
-            <Label
-              angle={270}
-              position="left"
-              style={{ textAnchor: 'middle', fill: theme.palette.text.primary }}
-            >
-              Sales ($)
-            </Label>
-          </YAxis>
-          <Line type="monotone" dataKey="amount" stroke={theme.palette.primary.main} dot={false} />
-        </LineChart>
-      </ResponsiveContainer>
-    </React.Fragment>
-  );
+export default class Example extends PureComponent {
+  static jsfiddleUrl = 'https://jsfiddle.net/alidingling/k9jkog04/';
+
+  render() {
+    return (
+      <PieChart width={400} height={400}>
+        <Pie dataKey="value" isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={80} fill="#8884d8" label />
+        <Pie dataKey="value" data={data02} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" />
+        <Tooltip />
+      </PieChart>
+    );
+  }
 }
