626789930e82aaf165c59bf1f718666eabdb2aa6
Carmina Cruz <carminacruz@Carminas-MacBook-Pro.local>
2020-03-10 00:07:39 -0700

    added PR_files

--- metrics-dashboard/src/components/Dashboard/Chart.js
+++ metrics-dashboard/src/components/Dashboard/Chart.js
@@ -4,16 +4,24 @@ import {
 } from 'recharts';
 import Title from './Title.js';
 
+// const data01 = [
+//   { name: 'Group A', value: 400 }, { name: 'Group B', value: 300 },
+//   { name: 'Group C', value: 300 }, { name: 'Group D', value: 200 },
+//   { name: 'Group E', value: 278 }, { name: 'Group F', value: 189 },
+// ];
+//
+// const data02 = [
+//   { name: 'Group A', value: 2400 }, { name: 'Group B', value: 4567 },
+//   { name: 'Group C', value: 1398 }, { name: 'Group D', value: 9800 },
+//   { name: 'Group E', value: 3908 }, { name: 'Group F', value: 4800 },
+// ];
+
 const data01 = [
   { name: 'Group A', value: 400 }, { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 }, { name: 'Group D', value: 200 },
-  { name: 'Group E', value: 278 }, { name: 'Group F', value: 189 },
 ];
 
 const data02 = [
   { name: 'Group A', value: 2400 }, { name: 'Group B', value: 4567 },
-  { name: 'Group C', value: 1398 }, { name: 'Group D', value: 9800 },
-  { name: 'Group E', value: 3908 }, { name: 'Group F', value: 4800 },
 ];
 
 export default class Example extends PureComponent {
