461b3d9eb6db4f22f43d2e7e3efe6390cd0149e7
Carmina Cruz <cminaacruz@gmail.com>
2020-05-24 22:53:13 -0700

    finished queries for 7 day and 30 day

--- pmpro/src/components/PieChart.js
+++ pmpro/src/components/PieChart.js
@@ -68,11 +68,31 @@ class PieChart extends React.Component {
   }
 
   async componentDidMount() {
-      const variables = {owner: "scrapy", name: "scrapy"};
-      const githubQuery = {query: "repo:scrapy/scrapy is:pr is:closed updated:>" + "2020-05-14"};
+      /* these consts are for the timeRangeQuery */
+      /* TODO: Still need to be able to pass the date */
+      const query1 = "repo:scrapy/scrapy is:pr is:open updated:>" + "2020-03-29 ";
+      const query2 = "repo:scrapy/scrapy is:pr is:closed updated:>" + "2020-03-29 ";
+      const query3 = "repo:scrapy/scrapy is:pr is:merged updated:>" + "2020-03-29 ";
+      const variables = {query1: query1, query2: query2, query3: query3};
+
+      const timeRangeQUery = `
+        query ($query1: String!, $query2: String!, $query3: String!) {
+            openedPR: search(query: $query1, type: ISSUE, last: 100) {
+                issueCount
+            }
+            closedPR: search(query: $query2, type: ISSUE, last: 100) {
+                issueCount
+            }
+            mergedPR: search(query: $query3, type: ISSUE, last: 100) {
+                issueCount
+            }
+        }`;
+
+      /* consts for allPRQuery */
+      const allPRVar = {owner: "scrapy", name: "scrapy"};
 
       const allPRQuery = `
-       query ($owner: String!, $name: String!) {
+        query ($owner: String!, $name: String!) {
             repository(owner: $owner, name: $name) {
                 openedPR: pullRequests(first: 100, states: OPEN) {
                     totalCount
@@ -86,27 +106,21 @@ class PieChart extends React.Component {
             }
         }`;
 
-      const withinLastMonth = `
-      {
-          search(query: $query, type: ISSUE, last: 100) {
-            issueCount
-            edges {
-              node {
-                ... on PullRequest {
-                  title
-                }
-              }
-            }
-          }
-        }`;
-
       try {
-          let result = await this.makeGraphQLQuery(allPRQuery, variables);
+          let result = await this.makeGraphQLQuery(timeRangeQUery, variables);
 
-          const openedPR = result.data.repository.openedPR.totalCount;
-          const closedPR = result.data.repository.closedPR.totalCount;
-          const mergedAndClosedPR = result.data.repository.closedPR.totalCount + result.data.repository.mergedPR.totalCount;
-          const totalPR = openedPR + mergedAndClosedPR;
+          // const openedPR = result.data.repository.openedPR.totalCount;
+          // const closedPR = result.data.repository.closedPR.totalCount;
+          // const mergedAndClosedPR = result.data.repository.closedPR.totalCount + result.data.repository.mergedPR.totalCount;
+          // const totalPR = openedPR + mergedAndClosedPR;
+
+          const openedPR = result.data.openedPR.issueCount;
+          const closedPR = result.data.closedPR.issueCount - result.data.mergedPR.issueCount;
+          const mergedAndClosedPR = result.data.mergedPR.issueCount;
+
+          const totalPR = openedPR + closedPR + mergedAndClosedPR;
+
+
 
           this.setState(prevState => ({
                         totalPR: totalPR,
