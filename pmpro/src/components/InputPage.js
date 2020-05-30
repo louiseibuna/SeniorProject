ba91da00be4190a839563d4a6aa9f2e8e79dbc17
louiseibuna <louisemibuna@gmail.com>
2020-05-30 12:15:34 -0700

    submit button and forms

--- pmpro/src/components/InputPage.js
+++ pmpro/src/components/InputPage.js
@@ -2,14 +2,18 @@ import React from "react";
 
 const InputPage = () => {
   return (
-    <div className="form-group">
-      <label htmlFor="formGroupExampleInput">GitHub Repository</label>
-      <input
-        type="text"
-        className="form-control"
-        id="formGroupExampleInput"
-      />
-    </div>
+    <form>
+      <div className="form-group">
+        <label htmlFor="formGroupExampleInput">GitHub Repository</label>
+          <input
+            type="text"
+            className="form-control"
+            id="formGroupExampleInput"
+          />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+    
   );
 }
 
